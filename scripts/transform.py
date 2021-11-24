11476afc38a580fe4a1e03d1fcd5f788e026b16d
Jasmine Shih <jasmineyshih@gmail.com>
2021-11-23 18:16:48 -0800

    fix another bug in transform script and commit current progress

--- scripts/transform.py
+++ scripts/transform.py
@@ -8,15 +8,15 @@ linkList = nodesAndLinks["links"]
 
 nodeDict = {}
 for nodeObj in nodeList:
-    nodeObj["parents"] = []
-    nodeObj["children"] = []
+    nodeObj["following"] = []
+    nodeObj["followers"] = []
     nodeDict[nodeObj["id"]] = nodeObj
 
 for linkObj in linkList:
     sourceId = linkObj["source"]
     targetId = linkObj["target"]
-    nodeDict[sourceId]["children"].append(targetId)
-    nodeDict[targetId]["parents"].append(sourceId)
+    nodeDict[sourceId]["following"].append(targetId)
+    nodeDict[targetId]["followers"].append(sourceId)
 
 with open("../data/nodesWithLinks.json", "w") as outfile:
     json.dump(list(nodeDict.values()), outfile, indent=4)
