7509d3929e39a232ad343feb4034b4e5476516b4
D M Raisul Ahsan <ahsan@DN0a1f0a11.SUNet>
2021-12-06 00:07:27 -0800

    Filtered data

--- scripts/tweeter_data_process.py
+++ scripts/tweeter_data_process.py
@@ -1,12 +1,30 @@
 import json
 
-posters_json = open("urban_dictionary_posters.json")
-posters = json.load(posters_json)
+tweets_json = open("urban_dictionary_tweets.json")
+tweets = json.load(tweets_json)
+
+user_ids = []
+
+
+for tweet in tweets:
+    tweet_text = tweet['tweet']
+    if tweet_text[0:2] != 'RT':
+        id = tweet['id']
+        if id not in user_ids:
+            user_ids.append(id)
 
 count  = 1
-for user in posters:
-    user['serial'] = count
+user_ids_json = []
+for id in user_ids:
+    data = {
+        'serial': count,
+        'id': id
+    }
+    user_ids_json.append(data)
     count += 1
 
-with open("user_ids.json", 'w') as outfile:
-            json.dump(posters, outfile, indent=4)
+
+
+
+with open("user_ids_excpt_RT.json", 'w') as outfile:
+            json.dump(user_ids_json, outfile, indent=4)
